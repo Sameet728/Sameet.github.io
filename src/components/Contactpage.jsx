01a5928fdad56b6935c9d384aa4bfd3fd8acb752
Sameet <125867736+Sameet728@users.noreply.github.com>
2024-09-13 16:39:57 +0530

    Add files via upload

--- src/components/Contactpage.jsx
+++ src/components/Contactpage.jsx
@@ -82,22 +82,22 @@ if(formData.number.length == 10 || formData.number.length == 0){
    <div id='contact' className=' pt-20 sm:mt-0 sm:h-[90vh] pl-0 pr-4 sm:pl-20 sm:pr-20'>
     
    <div className='flex flex-col items-center gap-2'>
-          <h2 className='text-1/2xl text-gray-500'>Get in Touch</h2>
-          <h2 className='text-4xl font-semibold'>Contact Me</h2>
+          <h2 id='animation' className='text-1/2xl text-gray-500'>Get in Touch</h2>
+          <h2 id='animation' className='text-4xl font-semibold'>Contact Me</h2>
         </div>
 
 <div className='flex flex-col-reverse sm:flex-row '>
   <div className=' hidden sm:flex w-2/5 items-center justify-center gap-10 mt-24 flex-col'>
-  <h2 className='text-3xl font-semibold text-gray-500  '><span className='text-gray-500'>📞</span> Get in Touch</h2>
-  <div className='flex items-center border border-gray-500 rounded-xl  hover:scale-105 transition-all w-1/2 p-2 mt-5 gap-2 '>
+  <h2 id='animation' className='text-3xl font-semibold text-gray-500  '><span className='text-gray-500'>📞</span> Get in Touch</h2>
+  <div id='animation' className='flex items-center border border-gray-500 rounded-xl  hover:scale-105 transition-all w-1/2 p-2 mt-5 gap-2 '>
     <img src="./email.png" alt="" className='w-[50px]' />
     <span className='hover:underline hover:underline-offset-8 cursor-pointer hover:text-gray-500  '>sameetpisal@gmail.com</span>
   </div>
-  <div className='flex gap-2'>
+  <div id='animation' className='flex gap-2'>
     <h2 className='text-xl font-bold'>Address :-</h2>
     <span className='text-xl font-semibold text-gray-600'>Pune, Maharashtra, India</span>
   </div>
-  <div className='flex gap-12  mt-10  items-center'>
+  <div id='animation' className='flex gap-12  mt-10  items-center'>
              <a href="https://github.com/Sameet728?tab=repositories" target='_blank'> <FaGithub className='text-3xl cursor-pointer hover:scale-125 transition-all' /></a> 
              <a href="https://www.linkedin.com/in/sameet-pisal-2a597b217/" target='_blank'> <FaLinkedin className='text-3xl cursor-pointer hover:scale-125 transition-all' /></a> 
              <a href="mailto:sameetpisal@gmail.com" target='_blank'> <IoMdMail className='text-3xl cursor-pointer hover:scale-125 transition-all' /></a> 
@@ -105,29 +105,29 @@ if(formData.number.length == 10 || formData.number.length == 0){
   
   </div>
   
-  <div className='sm:w-3/5 mt-12 p-4 '>
-  <h2 className=' font-semibold text-gray-600 text-2xl sm:pl-10 sm:text-3xl'>Love to hear from you 👋</h2>
+  <div className='sm:w-3/5 mt-12 pt-4 pb-5 flex flex-col items-center justify-center'>
+  <h2 id='animation' className=' font-semibold text-gray-600 text-2xl sm:pl-10 sm:text-3xl'>Love to hear from you 👋</h2>
   
   <div className='mt-10'>
     <div className='flex flex-col sm:flex-row   gap-8 '>
-    <div>
+    <div id='animation'>
       <label htmlFor="name" className='font-semibold text-gray-900'>Your Name</label>
-     <Input required onChange={handleInputChange} value={formData.name} name="name" className='w-[400px] sm:w-[440px] text-1/2xl font-bold mt-1 bg-gray-50' placeholder="Your full name, please" type="text"/>
+     <Input required onChange={handleInputChange} value={formData.name} name="name" className='w-[105%]  sm:w-[440px] text-1/2xl font-bold mt-1 bg-gray-50' placeholder="Your full name, please" type="text"/>
     </div>
-    <div>
+    <div id='animation'>
     <label htmlFor="email" className='font-semibold text-gray-900 '>Email</label>
-     <Input onChange={handleInputChange}  value={formData.email} name="email" className='w-[400px] sm:w-[440px] text-1/2xl font-bold mt-1 bg-gray-50' placeholder="Drop your email address"  type="email"/>
+     <Input onChange={handleInputChange}  value={formData.email} name="email" className='w-[105%]  sm:w-[440px] text-1/2xl font-bold mt-1 bg-gray-50' placeholder="Drop your email address"  type="email"/>
     </div>
     </div>
-    <div className='flex flex-col sm:flex-row    gap-8 mt-8'>
-    <div>
+    <div  className='flex flex-col sm:flex-row    gap-8 mt-8'>
+    <div id='animation'>
       <label htmlFor="name" className='font-semibold text-gray-900'>Phone Number (optional)</label>
-     <Input onChange={handleInputChange}  value={formData.number} name="number" className='w-[400px] sm:w-[440px] text-1/2xl font-bold mt-1 bg-gray-50' placeholder="Leave your number, if you'd like" type="number"/>
+     <Input onChange={handleInputChange}  value={formData.number} name="number" className='w-[105%]  sm:w-[440px] text-1/2xl font-bold mt-1 bg-gray-50' placeholder="Leave your number, if you'd like" type="number"/>
     </div>
-    <div> 
+    <div id='animation'> 
     <label htmlFor="name" className='font-semibold text-gray-900'>Select your preferred contact method  </label>
     <Select onValueChange={handleSelectChange}>
-  <SelectTrigger className="w-[400px] sm:w-[440px] text-1/2xl font-semibold  mt-1 bg-gray-50 ">
+  <SelectTrigger className="w-[105%]  sm:w-[440px] text-1/2xl font-semibold  mt-1 bg-gray-50 ">
     <SelectValue  placeholder="Email or phone?"  />
   </SelectTrigger>
   <SelectContent>
@@ -140,14 +140,14 @@ if(formData.number.length == 10 || formData.number.length == 0){
 
     </div>
     </div>
-    <div className='mt-8 '>
+    <div id='animation' className='mt-8 '>
     <label htmlFor="message" className='font-semibold text-gray-900 '>Message</label>
-    <Textarea onChange={handleInputChange}  value={formData.message} name="message" className=' text-1/2xl font-bold mt-1 bg-gray-50 w-[400px] sm:w-[920px]' placeholder="Feel free to share the details here"></Textarea>
+    <Textarea onChange={handleInputChange}  value={formData.message} name="message" className=' text-1/2xl font-bold mt-1 bg-gray-50 w-[105%]  sm:w-[920px]' placeholder="Feel free to share the details here"></Textarea>
      
     </div>
 
   </div>
-  <div className='flex justify-center sm:justify-end items-center sm:pr-36 pt-10'><Button onClick={sendHandler}>{loading ? <><FaSpinner  className="animate-spin mr-2 text-white" />
+  <div id='animation' className='flex justify-center sm:justify-end items-center sm:pr-36 pt-10'><Button onClick={sendHandler}>{loading ? <><FaSpinner  className="animate-spin mr-2 text-white" />
     Please Wait</>  : "Just Send 🡵"}</Button></div>
   </div>
 
